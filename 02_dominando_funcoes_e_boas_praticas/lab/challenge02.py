0a1d3b805f70fbecca0be0082843310edb5c6059
Paulo Henrique Reis <phdsreis@gmail.com>
2025-10-31 18:10:41 -0300

    desafios de código

---
+++ 02_dominando_funcoes_e_boas_praticas/lab/challenge02.py
@@ -0,0 +1,36 @@
+from unittest import case
+
+
+def identificar_categoria_gadget(codigo):
+    """
+    Recebe uma string 'codigo' e retorna a categoria associada:
+    - 'T': tablet
+    - 'P': phone
+    - 'N': notebook
+    Se não corresponder, retorna 'unknown'.
+    """
+    # TODO: Implemente a lógica para identificar a categoria do gadget
+    # Dica: Verifique a primeira letra do código para determinar a categoria
+    # Tipo esperado: 'codigo' é uma string não vazia
+
+    # Remova este pass ao implementar a solução
+    first_letter = codigo[0].upper()
+
+    match (first_letter):
+        case 'T':
+            return 'tablet'
+        case 'P':
+            return 'phone'
+        case 'N':
+            return 'notebook'
+        case _:
+            return 'unknown'
+
+
+# Leitura da entrada (espera-se uma string representando o código do gadget)
+codigo_gadget = input().strip()
+
+# Chamada da função e saída do resultado
+categoria = identificar_categoria_gadget(codigo_gadget)
+
+print(categoria)  # Deve imprimir uma das categorias ou 'unknown'
